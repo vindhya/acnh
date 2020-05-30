5dc1409f486efdf5d80ddff97b8eb33344f4f7a3
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2020-05-30 10:22:42 -0400

    trying auth0

--- web/src/index.js
+++ web/src/index.js
@@ -1,3 +1,5 @@
+import { AuthProvider } from '@redwoodjs/auth'
+import Auth0Client from '@auth0/auth0-spa-js'
 import ReactDOM from 'react-dom'
 import { RedwoodProvider, FatalErrorBoundary } from '@redwoodjs/web'
 import FatalErrorPage from 'src/pages/FatalErrorPage'
@@ -7,11 +9,21 @@ import Routes from 'src/Routes'
 import './scaffold.css'
 import './index.css'
 
+const auth0 = new Auth0Client({
+  domain: process.env.AUTH0_DOMAIN,
+  client_id: process.env.AUTH0_CLIENT_ID,
+  redirect_uri: 'http://localhost:8910/',
+  cacheLocation: 'localstorage',
+  audience: process.env.AUTH0_AUDIENCE,
+})
+
 ReactDOM.render(
   <FatalErrorBoundary page={FatalErrorPage}>
-    <RedwoodProvider>
-      <Routes />
-    </RedwoodProvider>
+    <AuthProvider client={auth0} type="auth0">
+      <RedwoodProvider>
+        <Routes />
+      </RedwoodProvider>
+    </AuthProvider>
   </FatalErrorBoundary>,
   document.getElementById('redwood-app')
 )
