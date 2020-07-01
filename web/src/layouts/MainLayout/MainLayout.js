c031c061afea581707b062bc29ce29882f6806c8
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2020-07-01 18:19:33 -0400

    refactors styling :nail_care:

--- web/src/layouts/MainLayout/MainLayout.js
+++ web/src/layouts/MainLayout/MainLayout.js
@@ -1,17 +1,18 @@
 import { Link, routes } from '@redwoodjs/router'
 
 import ChevronDownIcon from 'src/assets/chevron-down-solid.svg'
-import './MainLayout.css'
 
 const MainLayout = ({ children }) => {
   return (
     <>
       <header className="max-w-screen-xl mx-auto pt-20 pb-12">
-        <h1 className="text-3xl">
+        <h1 className="color-paragraph text-2xl">
           <Link to={routes.home()}>Animal Crossing: New Horizons</Link>
         </h1>
-        <h2 className="text-6xl">Critters you can catch right now</h2>
-        <ChevronDownIcon className="icon w-12 mt-4" />
+        <h2 className="color-headline-dark text-5xl font-black">
+          Critters you can catch right now
+        </h2>
+        <ChevronDownIcon className="color-accent w-10 mt-4" />
       </header>
       <main>{children}</main>
     </>
