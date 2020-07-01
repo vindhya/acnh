215b9f1ce20d408dd8af644a2403be1c3bbba2a1
Vindhya Raviraj <vindhya.raviraj@gmail.com>
2020-07-01 16:27:56 -0400

    more config to get tailwind to work

---
+++ web/config/webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = (config) => {
+  config.module.rules[0].oneOf[5] = {
+    test: /\.css$/,
+    sideEffects: true,
+    use: [
+      'style-loader',
+      { loader: 'css-loader', options: { importLoaders: 1 } },
+      { loader: 'postcss-loader', options: { config: { path: __dirname } } },
+    ],
+  }
+
+  return config
+}
